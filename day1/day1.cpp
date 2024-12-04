580952015285bc63ed8d18ce88c9bfa43fa9db53
Jackieto <jackietoyt@gmail.com>
2024-12-04 18:08:05 +0100

    day 1 part 1 completed

---
+++ day1/day1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+
+
+
+
+int main(){
+
+    std::ifstream input;
+    input.open("day1_input.txt");
+    int suma = 0;
+
+    int array1[6];
+    int array2[6];
+
+    int counter = 0;
+    int n;
+
+    std::vector<int> v1;
+    std::vector<int> v2;
+
+    while(input){
+        input >> n;
+        v1.push_back(n);
+        input >> n;
+        v2.push_back(n);
+    }
+
+    std::sort(v1.begin(),v1.end());
+    std::sort(v2.begin(),v2.end());
+
+    for(int i = 0; i < v1.size(); i++){
+        suma += std::abs(v1[i] - v2[i]);
+    }
+
+    std::cout << "Suma final: " << suma  <<std::endl;;
+
+    return 0;
+
+
+
+}
