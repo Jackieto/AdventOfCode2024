fc64a1fcec01e578fa6ad69f82bba5e967fb24e8
Jackieto <jackietoyt@gmail.com>
2024-12-11 11:46:00 +0100

    day 2 part 2 completed

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -4,9 +4,31 @@
 #include <cstdlib>
 #include <algorithm>
 #include <iterator>
+#include <sstream>
 
-,
-bool valid_distances(std::vector<int> v){
+void checkSorting(const std::vector<int>& v, bool& isAscending, bool& isDescending){
+
+    isAscending = true;
+    isDescending = true;
+
+   for (size_t i = 1; i < v.size(); ++i) {
+        if (v[i - 1] > v[i]) {
+            isAscending = false; // No está en orden ascendente
+        }
+        if (v[i - 1] < v[i]) {
+            isDescending = false; // No está en orden descendente
+        }
+        /*
+        // Si ambas condiciones son falsas, podemos salir temprano
+        if (!isAscending && !isDescending) {
+            break;
+        }
+        */
+    }
+    
+}
+
+bool valid_distances(const std::vector<int>& v){
     
     for(auto it = v.begin(); it < v.end() - 1; it++){
         int distance = std::abs(*it - *(it+1));
@@ -21,6 +43,32 @@ bool valid_distances(std::vector<int> v){
 
 }
 
+//PART2
+ bool safeAfterToleration(const std::vector<int>& v){
+
+    if (v.size() <= 1) {
+    return false; // Un vector con 1 o 0 elementos no puede ser ascendente ni descendente tras eliminación
+}
+    for(int i = 0; i < v.size(); i++){
+        std::vector<int> tmp(v);
+        tmp.erase(tmp.begin() + i);
+        bool isAscending = true;
+        bool isDescending = true;
+
+        checkSorting(tmp,isAscending,isDescending);
+        if(isAscending || isDescending){
+            if(valid_distances(tmp)){
+                return true;
+            }
+        }
+    }
+
+
+    return false;
+
+
+ }
+
 
 int main(){
 
@@ -30,17 +78,36 @@ int main(){
 
     std::vector<int> v;
     int n;
-    for(int i = 0; i < 7; i++){
-        input >> n;
-        v.push_back(n);
-    }
 
-    std::vector<int> v2 = {1, 2, 7, 8, 9};
+     std::string line;
 
-    std::cout << valid_distances(v2);
+     int safeReports = 0;
+
+    // Leer el archivo línea por línea
+    while (std::getline(input, line)) {
+        std::vector<int> v;
+        std::istringstream lineStream(line); // Convertir la línea en un flujo de entrada
+        int number;
+        while (lineStream >> number) {
+            v.push_back(number); // Agregar cada número al vector
+        }
+        
+        bool isAscending = true;
+        bool isDescending = true;
+        checkSorting(v,isAscending,isDescending);
+        if((isAscending || isDescending) && valid_distances(v)){
+          
+                safeReports++;
+        }else if(safeAfterToleration(v)){
+                safeReports++;
+            }
+        }
 
 
 
-  
+    std::cout << "Safe Report Count: "<<safeReports<<std::endl;
+
+
+
     return 0;
 }
